49419a825b236c82767f82b50f1acc7aadcf8dec
Francisco Perez <f.perez475@gmail.com>
2020-03-31 23:15:31 +0200

    refactor social

--- behavior_suite/ui/gui/views/social.py
+++ behavior_suite/ui/gui/views/social.py
@@ -25,34 +25,27 @@ class SocialMedia(QWidget):
         self.setMaximumHeight(30)
         self.setContentsMargins(0,0,0,0)
         main_layout.setContentsMargins(0,0,0,0)
-        # self.setFixedHeight(60)
 
-        pmap = QPixmap(':/assets/github.png').scaled(50, 50, Qt.KeepAspectRatio)
-        logo_label = ClickableLabel('https://github.com/jderobot/behaviorsuite')
-        logo_label.setPixmap(pmap)
-        logo_label.setFixedSize(30, 30)
-        logo_label.setScaledContents(True)
-        logo_label.setOpenExternalLinks(True)
-
-        pmap1 = QPixmap(':/assets/youtube.png').scaled(50, 50, Qt.KeepAspectRatio)
-        logo_label1 = ClickableLabel('https://www.youtube.com/channel/UCgmUgpircYAv_QhLQziHJOQ')
-        logo_label1.setPixmap(pmap1)
-        logo_label1.setFixedSize(30, 30)
-        logo_label1.setScaledContents(True)
-
-        pmap2 = QPixmap(':/assets/twitter.png').scaled(50, 50, Qt.KeepAspectRatio)
-        logo_label2 = ClickableLabel('https://twitter.com/jderobot')
-        logo_label2.setPixmap(pmap2)
-        logo_label2.setFixedSize(30, 30)
-        logo_label2.setScaledContents(True)
+        github_label = self.create_button('https://github.com/jderobot/behaviorsuite', ':/assets/github.png')
+        youtube_label = self.create_button('https://www.youtube.com/channel/UCgmUgpircYAv_QhLQziHJOQ', ':/assets/youtube.png')
+        twitter_label = self.create_button('https://twitter.com/jderobot', ':/assets/twitter.png')
 
         horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         main_layout.addItem(horizontalSpacer)
-        main_layout.addWidget(logo_label, alignment=Qt.AlignRight)
-        main_layout.addWidget(logo_label1, alignment=Qt.AlignRight)
-        main_layout.addWidget(logo_label2, alignment=Qt.AlignRight)
+        main_layout.addWidget(github_label, alignment=Qt.AlignRight)
+        main_layout.addWidget(youtube_label, alignment=Qt.AlignRight)
+        main_layout.addWidget(twitter_label, alignment=Qt.AlignRight)
 
         self.setLayout(main_layout)
 
         self.show()
+
+    def create_button(self, url, pixmap):
+        pmap2 = QPixmap(pixmap).scaled(50, 50, Qt.KeepAspectRatio)
+        logo = ClickableLabel(url)
+        logo.setPixmap(pmap2)
+        logo.setFixedSize(30, 30)
+        logo.setScaledContents(True)
+
+        return logo
