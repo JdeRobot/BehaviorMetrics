e8f08096257d808c5280ed16b8a2f6d192adab37
Diego Charrez <dcharrezt@unsa.edu.pe>
2020-06-30 00:00:10 -0500

    improved docker guide

--- .docker/noetic/vnc_startup.sh
+++ .docker/noetic/vnc_startup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Make sure there aren't ohther displays running before launch vnc.
 rm -rfv ~/tmp
@@ -9,8 +9,10 @@ Xvfb :0 -screen 0 1920x1080x24 &
 icewm-session &
 x11vnc -display :0 -passwd jderobot -forever -noxdamage &
 
-
 # Source noetic
 source ~/.bashrc
+
+# Start jupyter lab
+DISPLAY=:0 jupyter lab --no-browser --ip 0.0.0.0 --port 8888 --allow-root &
 
 exec "$@"
