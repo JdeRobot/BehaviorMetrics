d20ff3143babd2e12399abb004af88bdfc2aca4f
sergiopaniego <sergiopaniegoblanco@gmail.com>
2020-12-10 14:40:18 +0000

    Documentation improved

--- behavior_studio/utils/metrics.py
+++ behavior_studio/utils/metrics.py
@@ -1,8 +1,26 @@
-from bagpy import bagreader
+#!/usr/bin/env python
+
+"""This module contains the metrics manager.
+
+This module is in charge of generating metrics for a brain execution.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import pandas as pd
 import numpy as np
-from datetime import datetime
 import shutil
+
+from datetime import datetime
+from bagpy import bagreader
 
 
 def is_finish_line(point, start_point):
--- behavior_studio/utils/script_manager.py
+++ behavior_studio/utils/script_manager.py
@@ -1,8 +1,28 @@
-from utils import environment
+#!/usr/bin/env python
+
+"""This module contains the script manager.
+
+This module is in charge of running the application as an script, without GUI. 
+It is used for experiments for different brains/worlds
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 import subprocess
 import xml.etree.ElementTree as ET
 import time
 import os
+
+from utils import environment
 from utils.logger import logger
 from pilot import Pilot
 
