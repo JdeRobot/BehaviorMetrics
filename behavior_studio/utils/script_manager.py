a12574a00fbbf3f2c3afa5a5d1f1e6631719b60e
sergiopaniego <sergiopaniegoblanco@gmail.com>
2020-12-21 15:58:13 +0000

    Run experiment until 100% lap is completed or time exceeded

--- behavior_studio/utils/script_manager.py
+++ behavior_studio/utils/script_manager.py
@@ -79,8 +79,9 @@ def run_brains_worlds(app_configuration, controller):
             perfect_lap_checkpoints, circuit_diameter = metrics.read_perfect_lap_rosbag('lap-simple-circuit.bag')
             point = np.array([controller.pilot.sensors.get_pose3d('pose3d_0').getPose3d().x, controller.pilot.sensors.get_pose3d('pose3d_0').getPose3d().y])
             
-            while rospy.get_time() - time_start < app_configuration.experiment_timeout:
-                rospy.sleep(5)
+            
+            while (rospy.get_time() - time_start < app_configuration.experiment_timeout and not metrics.is_finish_line_2(point, perfect_lap_checkpoints[0])) or rospy.get_time() - time_start < 10:
+                rospy.sleep(0.1)
                 point = np.array([controller.pilot.sensors.get_pose3d('pose3d_0').getPose3d().x, controller.pilot.sensors.get_pose3d('pose3d_0').getPose3d().y])
 
             
