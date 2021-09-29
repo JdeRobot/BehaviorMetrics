905c43e8e4f673832728101501f373c71ee40619
sergiopaniego <sergiopaniegoblanco@gmail.com>
2021-09-29 15:42:43 +0000

    Show mean camera deviation

--- behavior_metrics/ui/gui/views/stats_window.py
+++ behavior_metrics/ui/gui/views/stats_window.py
@@ -15,7 +15,9 @@ class StatsWindow(QMainWindow):
         self.layout.addWidget(self.percentage_completed_label)
         self.completed_distance_label = QLabel("Completed distance -> " + str(self.controller.lap_statistics['completed_distance']) + "m")
         self.layout.addWidget(self.completed_distance_label)
-        
+        self.camera_deviation_error_label = QLabel("Mean camera deviation -> " + str(sum(self.controller.metrics['camera_deviation_error'])/len(self.controller.metrics['camera_deviation_error'])))
+        self.layout.addWidget(self.camera_deviation_error_label)
+
         # If lap is completed, extend information
         if 'lap_seconds' in self.controller.lap_statistics:
             self.lap_seconds_label = QLabel("Lap seconds -> " + str(self.controller.lap_statistics['lap_seconds']) + "s")
