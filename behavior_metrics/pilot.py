f815b3bd96fe914e029e77ff5cacdee861a5a432
Francisco Rivas <chanfr@hotmail.com>
2022-02-15 16:18:13 +0100

    Fixed tmp random initial index selection and non valid stat selection

--- behavior_metrics/pilot.py
+++ behavior_metrics/pilot.py
@@ -215,7 +215,10 @@ class Pilot(threading.Thread):
         if hasattr(self.brains.active_brain, 'inference_times'):
             self.brains.active_brain.inference_times = self.brains.active_brain.inference_times[10:-10]
             mean_inference_time = sum(self.brains.active_brain.inference_times) / len(self.brains.active_brain.inference_times)
-            frame_rate = self.sensors.get_camera('camera_0').total_frames / experiment_metrics['experiment_total_simulated_time']
+            try:
+                frame_rate = self.sensors.get_camera('camera_0').total_frames / experiment_metrics['experiment_total_simulated_time']
+            except:
+                frame_rate = 0
             gpu_inference = self.brains.active_brain.gpu_inference
             real_time_update_rate = self.real_time_update_rate
             first_image = self.brains.active_brain.first_image
