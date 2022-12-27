34df325360590e5c067c47050ca8d72cc5128963
sergiopaniego <sergiopaniegoblanco@gmail.com>
2022-12-27 10:14:56 +0100

    Show number of attempts for each experiment

--- behavior_metrics/driver_carla.py
+++ behavior_metrics/driver_carla.py
@@ -127,12 +127,16 @@ def main():
         pilot.kill_event.set()
         environment.close_ros_and_simulators()
     else:
+        experiments_information = {'world_counter': {}}
         for world_counter, world in enumerate(app_configuration.current_world):
+            experiments_information['world_counter'][world_counter] = {'brain_counter': {}}
             for brain_counter, brain in enumerate(app_configuration.brain_path):
+                experiments_information['world_counter'][world_counter]['brain_counter'][brain_counter] = {'repetition_counter': {}}
                 for repetition_counter in range(app_configuration.experiment_repetitions):
                     success = -1
                     experiment_attempts = 0
                     while success != 0:
+                        experiments_information['world_counter'][world_counter]['brain_counter'][brain_counter]['repetition_counter'][repetition_counter] = experiment_attempts
                         logger.info("Launching: python3 script_manager_carla.py -c configs/default_carla_multiple.yml -s -world_counter " + str(world_counter) + " -brain_counter " + str(brain_counter) + " -repetition_counter " + str(repetition_counter))
                         success = os.system("python3 script_manager_carla.py -c configs/default_carla_multiple.yml -s -world_counter " + str(world_counter) + " -brain_counter " + str(brain_counter) + " -repetition_counter " + str(repetition_counter))
                         if success != 0:
@@ -143,12 +147,13 @@ def main():
                                     logger.info("Removing empty folder: " + folder[0])
                                     os.rmdir(folder[0])
                         if success != 0 and experiment_attempts <= 5:
+                            experiment_attempts += 1
                             logger.info("Python process finished with error! Repeating experiment")
                         elif success != 0 and experiment_attempts > 5:
                             success = 0
                             logger.info("Too many failed attempts for this experiment.")
                         logger.info("Python process finished.")
-
+                    logger.info(experiments_information)
     logger.info('DONE! Bye, bye :)')
                     
 
