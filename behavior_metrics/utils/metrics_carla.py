60658101b9dfbcb1e0a0941b6b44b919327580ab
sergiopaniego <sergiopaniegoblanco@gmail.com>
2023-01-11 16:46:40 +0100

    More metrics shown in map plot

--- behavior_metrics/utils/metrics_carla.py
+++ behavior_metrics/utils/metrics_carla.py
@@ -199,12 +199,15 @@ def get_position_deviation(experiment_metrics, checkpoints, map_waypoints, exper
     
     full_text = ''
     for key, value in experiment_metrics.items():
+        print(key, value)
         full_text += ' * ' + str(key) + ' : ' + str(value) + '\n'
-    plt.figtext(0.1, 0.01, full_text, wrap=True, horizontalalignment='left', fontsize=20)
+    plt.figtext(0.1, 0.01, full_text, wrap=True, horizontalalignment='left', fontsize=18)
 
     plt.grid(True)
     #plt.subplots_adjust(left=0.5)
     plt.subplots_adjust(bottom=0.4)
+
+    plt.title(experiment_metrics['experiment_model'], fontsize=25)
 
     fig.savefig(experiment_metrics_filename + '.png', dpi=fig.dpi)
 
